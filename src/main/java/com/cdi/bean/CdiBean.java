62d4fe54fa0ee53913e8d1727c2f601989de9697
Krupnik_O <Krupnik_O@host.docker.internal>
2020-03-19 08:51:15 +0300

    some changes

--- src/main/java/com/cdi/bean/CdiBean.java
+++ src/main/java/com/cdi/bean/CdiBean.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Named
 @SessionScoped
@@ -11,8 +13,14 @@ public class CdiBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@NotNull
+	@Size(min = 3, message = "Login should at least be 3 characters long")
 	private String login;
+	
+	@NotNull
+	@Size(min = 5, max = 10, message = "Please enter a valid password (5-10 characters)")
 	private String password;
+	
 	private String logged;
 
 	public String getLogin() {
